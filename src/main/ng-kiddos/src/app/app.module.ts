71c51bfd8429708e6ace11a7ca95e98b963acbe7
darshanawagh <darshuhalo@gmail.com>
2018-08-30 11:44:03 +0000

    Added routing and admission page

--- src/main/ng-kiddos/src/app/app.module.ts
+++ src/main/ng-kiddos/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import { PaymentComponent } from './payment/payment.component';
+import { ProgramComponent } from './program/program.component';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { StaffComponent } from './staff/staff.component';
+import { TransportComponent } from './transport/transport.component';
+import { AdmissionComponent } from './admission/admission.component';
+import { ChildComponent } from './child/child.component';
+import { HomeComponent } from './home/home.component';
+import { AdmissionService } from './admission/admission.service';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentComponent,
+    ProgramComponent,
+    StaffComponent,
+    TransportComponent,
+    AdmissionComponent,
+    ChildComponent,
+    HomeComponent   
   ],
   imports: [
-    BrowserModule
+    BrowserModule ,
+    AppRoutingModule,
+    TableModule
+    
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AdmissionService],
+  bootstrap: [AppComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
